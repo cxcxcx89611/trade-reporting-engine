34879eb7a4f92b321b5d377e1cc38365d38804cc
cxcxcx89611 <xichen@thegoodguys.com.au>
2021-10-10 15:54:22 +1100

    first commit

---
+++ src/test/java/com/au/vanguard/tradereportingengine/TradeReportingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.au.vanguard.tradereportingengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeReportingEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
